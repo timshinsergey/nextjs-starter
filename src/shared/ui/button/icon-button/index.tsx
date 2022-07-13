656f97b01cfb54288bb2e952c5677d21c3e9e0d1
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-07-13 17:01:07 +0400

    update icon button

--- src/shared/ui/button/icon-button/index.tsx
+++ src/shared/ui/button/icon-button/index.tsx
@@ -1,4 +1,4 @@
-import { FC, ComponentProps } from 'react'
+import { ComponentProps, forwardRef } from 'react'
 import cn from 'classnames'
 
 export interface IconButtonProps extends ComponentProps<'button'> {
@@ -6,28 +6,29 @@ export interface IconButtonProps extends ComponentProps<'button'> {
 	size?: 'small' | 'medium' | 'large'
 }
 
-const IconButton: FC<IconButtonProps> = ({
-	variant = 'primary',
-	disabled,
-	className,
-	children,
-	size = 'medium',
-	...props
-}): JSX.Element => {
-	return (
-		<button
-			disabled={disabled}
-			tabIndex={disabled ? -1 : 0}
-			className={cn(
-				'w-7 h-7 bg-white bg-opacity-80 p-0 inline-flex justify-center items-center rounded-full disabled:opacity-50 lg:w-14 lg:h-14',
-				className
-			)}
-			type='button'
-			{...props}
-		>
-			{children}
-		</button>
-	)
-}
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+	(
+		{ variant = 'primary', disabled, className, children, size = 'medium', ...props },
+		ref
+	): JSX.Element => {
+		return (
+			<button
+				ref={ref}
+				disabled={disabled}
+				tabIndex={disabled ? -1 : 0}
+				className={cn(
+					'w-7 h-7 bg-white bg-opacity-80 p-0 inline-flex justify-center items-center rounded-full disabled:opacity-50 lg:w-14 lg:h-14',
+					className
+				)}
+				type='button'
+				{...props}
+			>
+				{children}
+			</button>
+		)
+	}
+)
+
+IconButton.displayName = 'IconButton'
 
 export default IconButton
