71952a86574a99b445da6bb58f8d619bf7abb7e3
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-07-22 17:05:16 +0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,21 @@ module.exports = {
 	theme: {
 		extend: {
 			container: {
-				center: true,
-				padding: {
-					DEFAULT: '1rem',
-					sm: '3rem',
+				screens: {
+					'2xl': '1440px',
+					'3xl': '1920px',
 				},
+				center: true,
+				// with bugs
+				// not all breakpoints work
+				// padding: {
+				// 	DEFAULT: '1rem',
+				// 	sm: '3rem',
+				// 	sm: '3rem',
+				// 	lg: '3rem',
+				// 	xl: '4rem',
+				// 	'2xl': '4rem',
+				// },
 			},
 		},
 	},
