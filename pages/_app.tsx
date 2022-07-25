d8e733c1eac9bb70b90273a07ebecb118ee784b8
Sergey <33506866+timshinsergey@users.noreply.github.com>
2022-07-25 20:32:31 +0400

    add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import '@app/app.css'
 import type { ReactElement, ReactNode } from 'react'
+import Router from 'next/router'
 import { I18nProvider } from 'next-rosetta'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
+import NProgress from 'nprogress'
 import SEO from '@app/next-seo.config.json'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -13,6 +15,12 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 type AppPropsWithLayout = AppProps & {
 	Component: NextPageWithLayout
 }
+
+NProgress.configure({ showSpinner: false })
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 	// Use the layout defined at the page level, if available
